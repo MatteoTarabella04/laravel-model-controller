c21653e92d42ff2abf2eea6f436bc7f185da3d45
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-24 16:38:29 +0200

    add element to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,10 @@
-<header>
-    header
+<header class="bg-dark text-center py-4">
+    <nav class="nav justify-content-center pt-4 pb-1 ">
+        <a class="nav-link {{ Route::currentRouteName() === 'home' ? 'bg-danger text-white' : '' }}"
+            href="{{ route('home') }}" aria-current="page">Home</a>
+        <a class="nav-link {{ Route::currentRouteName() === 'about' ? 'bg-danger text-white' : '' }}"
+            href="{{ route('about') }}">About Us</a>
+        <a class="nav-link {{ Route::currentRouteName() === 'contacts' ? 'bg-danger text-white' : '' }}"
+            href="{{ route('contacts') }}">Contacts</a>
+    </nav>
 </header>
