ec5733fe797d738e236756ac359fd5fec7c04dac
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-24 17:47:49 +0200

    add page elements

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>about</h1>
+    <div class="p-5 bg-light rounded-3">
+        <div class="container py-5">
+            <h1 class="display-5 fw-bold">Laravel Movies DB</h1>
+            <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vero laborum incidunt!
+                Voluptate odit nam et perferendis sapiente nisi numquam quibusdam velit, illo quisquam ducimus delectus
+                deserunt voluptatum quia culpa. Sed necessitatibus iure perspiciatis sapiente amet dolores a cupiditate
+                expedita numquam? Id odio hic sint dolores fugit fuga animi, expedita fugiat, itaque doloremque perspiciatis
+                voluptatem minima aliquid repellat obcaecati. Accusamus inventore eveniet non nulla necessitatibus!
+                Molestias officia distinctio quis deleniti quas harum laudantium sint incidunt ut illo dolores eos ratione
+                maiores maxime rem doloremque,</p>
+            <button class="btn btn-primary btn-lg" type="button">Laravel Docs</button>
+        </div>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow">
+                        <img src="https://picsum.photos/300/200" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui enim voluptate rerum delectus
+                                magni aliquid necessitatibus, vitae ad recusandae dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
