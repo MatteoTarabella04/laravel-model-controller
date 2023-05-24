105beb20a31fc4203eaf70b697a1e7fb9c4dac65
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-24 16:38:19 +0200

    add element to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,42 @@
-<footer>
-    footer
+<footer class="bg-dark text-white mt-5">
+    <div class="container py-5">
+        <div class="row row-cols-3 g-3">
+            <div class="col">
+                <h3>Infos</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum at dolorum reprehenderit placeat
+                    ducimus autem in quam? Repellat voluptas id tenetur nihil laudantium quidem error, odit nobis porro
+                    recusandae. Illum et corporis minus placeat modi delectus pariatur a dolorum odit!
+                </p>
+            </div>
+            <div class="col">
+                <h3>Links</h3>
+                <ul>
+                    <li>
+                        <a href="#">Lorem, ipsum.</a>
+                    </li>
+                    <li>
+                        <a href="#">Lorem, ipsum.</a>
+                    </li>
+                    <li>
+                        <a href="#">Lorem, ipsum.</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Contacts</h3>
+                <ul>
+                    <li>
+                        TEL: 45548523
+                    </li>
+                    <li>
+                        MAIL: contacts@us.com
+                    </li>
+                    <li>
+                        FAX: 12334567890
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
